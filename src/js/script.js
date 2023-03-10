767cb7d494146ad08f12925e28165dd936de011f
Jonald Damaso <damasojonald@gmail.com>
2023-03-10 14:18:14 +0000

    Features: added media query, put text first as list

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,56 @@
 const darkside = [
     {
-        "title": "THE DARK SIDE",
         "name": "DARTH VADER",
+        "class_name": "vader",
         "photo": "./images/darth-vader.png"
     },
     {
-        "title": "THE DARK SIDE",
+        "name": "DARTH SIDIOUS",
+        "class_name": "sidious",
+        "photo": "./images/Darth-Sidious-darkside.png"
+    },
+    {
         "name": "ASAJJ VENTRESS",
+        "class_name": "asajj",
         "photo": "./images/Asajj-Ventress-darkside.png"
     },
     {
-        "title": "THE DARK SIDE",
         "name": "KYLO REN",
+        "class_name": "kylo",
         "photo": "./images/Kylo-ren-darkside.png"
     },
     {
-        "title": "THE DARK SIDE",
         "name": "SAVAGE OPRESS",
+        "class_name": "savage",
         "photo": "./images/Savage-Opress-darkside.png"
     }
 ];
 
 const theForce = [
     {
-        "title": "THE FORCE",
         "name": "YODA",
+        "class_name": "yoda",
         "photo": "./images/yoda-theForce.png"
     },
     {
-        "title": "THE FORCE",
         "name": "OBI WAN KINOBI",
+        "class_name": "obiwan",
         "photo": "./images/Obi-Wan-kenobi-theForce.png"
     },
     {
-        "title": "THE FORCE",
         "name": "ANAKIN SKYWALKER",
+        "class_name": "anakin",
         "photo": "./images/Anakin-skywalker-theForce.png"
     },
     {
-        "title": "THE FORCE",
         "name": "CHEWBACCA aka 'CHEWIE'",
+        "class_name": "chewie",
         "photo": "./images/chewbacca-theForce.png"
+    },
+    {
+        "name": "AHSOKA TANO",
+        "class_name": "ahsoka",
+        "photo": "./images/ahsoka-tano-theForce.png"
     }
 ];
 
@@ -51,23 +61,14 @@ darkside.forEach((result, index) => {
     const content = `
     <div class="card__container card__container--DarthVader darkside-${index}">
         <div class="card__body card__body--DarthVader">
-            <div class="card__title card__title--DarthVader">
-                <h2>${result.title}</h2>
+            <div class="card__name card__name--DarthVader">
+                <h4>${result.name}</h4>
             </div>
             <div class="card__image">
                 <img src="${result.photo}" alt="a photo of master yoda, looks like meditating" class="img__darkside-${index}">
             </div>
-            <div class="card__content">
-                <div class="card__name card__name-DarthVader">
-                    <h4>${result.name}</h4>
-                </div>
-                <p class="card__text card__text--Yoda">LOVE</p>
-                <p class="card__text card__text--Yoda">BEAUTY</p>
-                <p class="card__text card__text--Yoda">CSS THAT SCALES!</p>
-                <p class="card__text card__text--Yoda">GREAT WARRIOR. WARS NOT MAKE ONE GREAT!</p>
-            </div>
             <div class="card__button">
-                <button class="button buttonDanger">Dark</button>
+                <button class="button buttonDanger button${result.class_name}">Dark</button>
             </div>
         </div>
     </div>
@@ -82,23 +83,14 @@ theForce.forEach((result, index) => {
     const content = `
     <div class="card__container card__container--Yoda theForce-${index}">
         <div class="card__body card__body--Yoda">
-            <div class="card__title card__title--Yoda">
-                <h2>${result.title}</h2>
+            <div class="card__name card__name--Yoda">
+                <h4>${result.name}</h4>
             </div>
             <div class="card__image">
                 <img src="${result.photo}" alt="a photo of master yoda, looks like meditating" class="img__theForce-${index}">
             </div>
-            <div class="card__content">
-                <div class="card__name card__name-Yoda">
-                    <h4>${result.name}</h4>
-                </div>
-                <p class="card__text card__text--Yoda">LOVE</p>
-                <p class="card__text card__text--Yoda">BEAUTY</p>
-                <p class="card__text card__text--Yoda">CSS THAT SCALES!</p>
-                <p class="card__text card__text--Yoda">GREAT WARRIOR. WARS NOT MAKE ONE GREAT!</p>
-            </div>
             <div class="card__button">
-                <button class="button buttonWarning">Force</button>
+                <button class="button buttonWarning button${result.class_name}">Force</button>
             </div>
         </div>
     </div>
